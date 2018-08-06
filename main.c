aeffa192d72754f909c32e72d1f2836e176fad8a
John Hilliard <praetoriansentry@gmail.com>
2018-08-06 21:58:25 +0000

    minor changes

--- main.c
+++ main.c
@@ -10,13 +10,14 @@ int main() {
     printf("Unable to open word list: %s\n", WORD_LIST);
     exit(1);
   }
-  char* dice = NULL;
-  char* word = NULL;
+  char* dice = (char*) calloc(5, sizeof(char));
+  char* word = (char*) calloc(10, sizeof(char));
   char** words = (char**) calloc(MAX_WORDS, sizeof(char**));
   int current_word = 0;
-  while(fscanf(f, "%ms\t%ms\n", &dice, &word) == 2) {
+  while(fscanf(f, "%s\t%s\n", dice,word) == 2) {
     words[current_word++] = word;
     // printf("%s\t%s\n", dice, word);
+    word = (char*) calloc(10, sizeof(char));
   }
 
   srand(time(0));
@@ -26,5 +27,10 @@ int main() {
   printf("%s ", words[rand() % current_word]);
   printf("%s\n", words[rand() % current_word]);
 
-  return 1;
+  for(int i = 0; i < current_word; i = i + 1) {
+    free(words[i]);
+  }
+  free(dice);
+  free(words);
+  return 0;
 }
