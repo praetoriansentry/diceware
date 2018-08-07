d7be0bbc6bdcc0d18fe0f5bd8791f0a82b1a2f7c
John Hilliard <jhilliard@nextjump.com>
2018-08-07 12:45:39 -0400

    adding some more options

--- main.c
+++ main.c
@@ -2,24 +2,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <ctype.h>
 
 #define WORD_LIST "eff_large_wordlist.txt"
 #define MAX_WORDS 10000
 
+void show_help() {
+  printf("Diceware Passphrase Generator\n\n");
+  printf("Basic usage:\n");
+  printf("\t$ diceware\n\n");
+  printf("Optional Arguments:\n");
+  printf("\t-w\tThe number of words to use in each passphrase\n");
+  printf("\t-p\tThe number of passphrases to generate\n");
+  printf("\t-d\tThe delimiter to use between words. Defaults to space.\n");
+  printf("\t-c\tCapitalize the first letter of each word.\n");
+  printf("\n");
+  printf("Example:\n");
+  printf("\t$ diceware -w 5 -p 1 -c -d ''\n");
+  return;
+}
+
 int main(int argc, char **argv) {
-  int n_flag = 5;
+  int w_flag = 5;
   int p_flag = 1;
+  int c_flag = 0;
+  char* d_flag = " ";
   int c;
-  while ((c = getopt (argc, argv, "n:p:")) != -1) {
+  while ((c = getopt (argc, argv, "w:p:hd:c")) != -1) {
     switch (c) {
-    case 'n':
-      n_flag = atoi(optarg);
+    case 'c':
+      c_flag = 1;
+      break;
+    case 'w':
+      w_flag = atoi(optarg);
       break;
     case 'p':
       p_flag = atoi(optarg);
       break;
+    case 'd':
+      d_flag = optarg;
+      break;
+    case 'h':
+      show_help();
+      exit(0);
     default:
-      abort ();
+      show_help();
+      exit(0);
     }
   }
 
@@ -33,6 +61,9 @@ int main(int argc, char **argv) {
   char** words = (char**) calloc(MAX_WORDS, sizeof(char**));
   int current_word = 0;
   while(fscanf(f, "%s\t%s\n", dice,word) == 2) {
+    if (c_flag == 1) {
+      word[0] = toupper(word[0]);
+    }
     words[current_word++] = word;
     word = (char*) calloc(10, sizeof(char));
   }
@@ -41,11 +72,11 @@ int main(int argc, char **argv) {
   f = fopen("/dev/urandom", "r");
 
   for(int i = 0; i < p_flag; i = i + 1) {
-    for (int j = 0; j < n_flag; j = j + 1) {
+    for (int j = 0; j < w_flag; j = j + 1) {
       fread(&seed, 1, sizeof(unsigned int), f);
       printf("%s", words[seed % current_word]);
-      if (j < n_flag - 1) {
-        printf(" ");
+      if (j < w_flag - 1) {
+        printf("%s", d_flag);
       }
     }
     printf("\n");
